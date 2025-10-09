76e5e5f17a26666e011e23918b80a38a4835b6cb
dmytro <carlstremniy@gmail.com>
2025-10-09 03:05:03 +0300

    Enable test coverage and disable quality gate wait

--- NetSdrClientApp/obj/Debug/net8.0/NetSdrClientApp.AssemblyInfo.cs
+++ NetSdrClientApp/obj/Debug/net8.0/NetSdrClientApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetSdrClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb12e7581779b350f6c9fc0c38f9d4db757e2bec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47016803e10ca3f5d5cca264206c0cebf4bc36ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetSdrClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetSdrClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
